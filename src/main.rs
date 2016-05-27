1a37ff4c370bda26e64aa3e110143fbf359ca5fe
Luo <gnulinux@126.com>
2016-05-27 15:05:17 +0800

    add hyper.lib

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 #![feature(box_syntax)]
 #![allow(dead_code)]
 #[warn(unused_attributes)]
-
+extern crate hyper;
 
 mod jsonrpc;
 use jsonrpc::{ JsonRpc, RpcResult, RpcRequest, 
     json, ToJson };
+
+use std::io::{ copy, Read, Write };
+use std::sync::{ Arc, Mutex };
+use std::env;
+
+use hyper::server::{ Server, Request, Response };
+use hyper::method::Method::{ Get, Put, Post };
+use hyper::status::StatusCode; // { Ok, BadRequest, NotFound, MethodNotAllowed };
+
 
 // TODO: 给 RPC Method 增加 Share Memeory 支持.
 //          fn hello (sm: ShareMemoy , params: json::Json) -> RpcResult { }
@@ -35,7 +44,7 @@ fn ice (params: &Option<json::Json>) -> RpcResult {
     Ok( data.to_json() )
 }
 
-fn main (){
+fn test_rpc() {
     let mut rpc = JsonRpc::new();
     rpc.add_method("hello", Box::new(hello));
     rpc.add_method("ice", Box::new(ice));
@@ -48,4 +57,61 @@ fn main (){
             println!("Error:   {:?}", e);
         }
     }
+}
+
+
+fn main (){
+    let args: Vec<String> = env::args().collect();
+    println!("Args: {:?}", args);
+
+    // let mut rpc = JsonRpc::new();
+    // rpc.add_method("hello", Box::new(hello));
+    // rpc.add_method("ice", Box::new(ice));
+    // let share_rpc = Arc::new(Mutex::new(rpc));
+
+    Server::http("0.0.0.0:80").unwrap().handle(move |mut req: Request, mut res: Response| {
+        match req.method {
+            Post | Put => {
+                println!("{:?}", req.headers);
+                let mut rpc = JsonRpc::new();
+                rpc.add_method("hello", Box::new(hello));
+                rpc.add_method("ice", Box::new(ice));
+
+                let mut body = String::new();
+                match req.read_to_string(&mut body){
+                    Ok(body_length) => {
+                        println!("Body length: {:?}\n{:?}", body_length, body);
+                        let mut res = &mut res.start().unwrap();
+                        match RpcRequest::new(&body) {
+                            Ok(rpc_request) => {
+                                let response_content = rpc.call(&rpc_request).to_json().to_string();
+                                res.write(response_content.as_bytes()).unwrap();
+                            },
+                            Err(rpc_error) => {
+                                println!("Error:   {:?}", rpc_error);
+                                res.write(rpc_error.to_json().to_string().as_bytes()).unwrap();
+
+                                // *res.status_mut() = StatusCode::MethodNotAllowed;
+                            }
+                        }
+                        // res.status_mut() = &mut StatusCode::Ok;
+                    },
+                    Err(e) => {
+                        println!("Bad Request. {:?}", e);
+                        let mut res = &mut res.start().unwrap();
+                        // res.status_mut() = StatusCode::BadRequest;
+                        res.write(e.to_string().as_bytes()).unwrap();
+                    }
+                }
+            },
+            Get => {
+                println!("{:?}", req.headers);
+                copy(&mut req, &mut res.start().unwrap()).unwrap();  
+            },
+            _ => {
+                println!("Method Not Allowed.");
+                *res.status_mut() = StatusCode::MethodNotAllowed;
+            }
+        };
+    }).unwrap();
 }
