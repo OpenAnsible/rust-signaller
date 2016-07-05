9a4e43b13f1698f3e28c639588a8c8615cacc56f
Luo <gnulinux@126.com>
2016-07-05 08:30:18 +0800

    rebuild

--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,92 @@
-#![allow(dead_code)]
-#[warn(unused_attributes)]
-extern crate hyper;
-extern crate jsonrpc2;
 
-use jsonrpc2::{ 
-    JsonRpc, RpcResult, Request, Response, 
-    Error as RpcError, Json, ToJson 
-};
+#[macro_use(debug, error, info, log, log_enabled, trace, warn)]
+extern crate log;
+extern crate env_logger;
+extern crate ws;
+extern crate bson;
 
-use std::io::{ copy, Read, Write };
-use std::sync::{ Arc }; // Mutex
-use std::env;
 use std::str::FromStr;
+use std::string::ToString;
+pub use std::collections::BTreeMap;
+pub use std::net::SocketAddr;
 
-use hyper::server::{ 
-    Server, Request as HyperRequest, 
-    Response as HyperResponse, 
-    Handler as HyperHandler
+use ws::{ // connect, listen
+    WebSocket, CloseCode, 
+    Handler, Handshake,
+    Message, Result, Error
 };
-use hyper::method::Method::{ Get, Put, Post };
-use hyper::status::StatusCode; // { Ok, BadRequest, NotFound, MethodNotAllowed };
+
+pub use ws::Sender;
+pub use ws::util::Token;
+pub use bson::oid::ObjectId;
+pub use rustc_serialize::json::{Json, ToJson, Object};
+pub use rustc_serialize::hex::ToHex;
+
+mod channels;
+mod peer;
+
+pub use channels::{Channel, Channels};
+pub use peer::{Peer, Peers};
 
 
-// TODO: 给 RPC Method 增加 Share Memeory 支持.
-//          fn hello (sm: ShareMemoy , params: Json) -> RpcResult { }
-fn hello (params: &Option<Json>) -> RpcResult {
-    match params.as_ref() {
-        Some(p) => {
-            println!("Params: {:?}", p );
-        },
-        None => {
-            println!("Params: Null" );
-        }
-    };
-    Ok("Hello World".to_json())
+struct Server {
+    out: Sender,
+    channels: Channels
 }
 
-fn ice (params: &Option<Json>) -> RpcResult {
-    match params.as_ref() {
-        Some(p) => {
-            println!("Params: {:?}", p );
-        },
-        None => {
-            println!("Params: Null" );
-        }
-    };
-    let data = vec![1,2,3,4];
-    Ok( data.to_json() )
-}
+impl Handler for Server {
+    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+        println!("Peer Addr: {:?} Token: {:?}", 
+            shake.remote_addr().unwrap().unwrap().to_string(), self.out.token() );
+        // println!("Peer Socket Addr: {:?}", shake.local_addr);
 
-struct MyHandler {
-    rpc: Arc<JsonRpc>
-}
-impl HyperHandler for MyHandler {
-    fn handle(&self, mut req: HyperRequest, mut res: HyperResponse) {
-        println!("{:?}", self.rpc.methods().len() );
-        match req.method {
-            Post | Put => {
-                println!("{:?}", req.headers);
-
-                let mut body = String::new();
-                match req.read_to_string(&mut body){
-                    Ok(body_length) => {
-                        println!("Body length: {:?}\n{:?}", body_length, body);
-                        let mut res = &mut res.start().unwrap();
-                        match Request::from_str(&body) {
-                            Ok(rpc_request) => {
-                                let response_content = self.rpc.call(&rpc_request).to_json().to_string();
-                                res.write(response_content.as_bytes()).unwrap();
-                            },
-                            Err(rpc_error) => {
-                                println!("Error:   {:?}", rpc_error);
-                                res.write(rpc_error.to_json().to_string().as_bytes()).unwrap();
-                                // *res.status_mut() = StatusCode::MethodNotAllowed;
-                            }
-                        }
-                        // res.status_mut() = &mut StatusCode::Ok;
-                    },
-                    Err(e) => {
-                        println!("Bad Request. {:?}", e);
-                        let mut res = &mut res.start().unwrap();
-                        // res.status_mut() = StatusCode::BadRequest;
-                        res.write(e.to_string().as_bytes()).unwrap();
-                    }
-                }
-            },
-            Get => {
-                println!("{:?}", req.headers);
-                copy(&mut req, &mut res.start().unwrap()).unwrap();  
-            },
-            _ => {
-                println!("Method Not Allowed.");
-                *res.status_mut() = StatusCode::MethodNotAllowed;
-            }
-        };
+        Ok(())
+    }
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("Server got message '{}'. ", msg);
+        self.out.send(msg)
+    }
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        println!("WebSocket closing by ({:?}) for ({:?}:{}) ", self.out.token(), code, reason);
+        // self.out.shutdown().unwrap();
+    }
+    fn on_error(&mut self, err: Error) {
+        println!("{:?}", err);
     }
 }
-unsafe impl Send for MyHandler { }
-unsafe impl Sync for MyHandler { }
 
+impl Server {
+    fn run(host: &str){
+        // listen(host, |out| { Server { out: out } })
+        let ws = WebSocket::new(|out|{
+            Server { out: out, channels: Channels::empty() }
+        });
+        match ws {
+            Ok(ws) => {
+                let _ = ws.listen(host);
+                println!("WebSocket Server running on {}", host);
+            },
+            Err(e) => println!("WebSocket Server Running Error ({:?})", e)
+        };
+    }
+    fn channels(&self) -> Channels {
+        self.channels
+    }
+    fn join(&self, channel_id: ObjectId, token: Option<String>, ) -> Result<Vec<Channels>> {
+        
+    }
+    fn leave(&self, channel_id: ObjectId) -> bool {
+        false
+    }
+    fn broadcast(&self, channel_id: ObjectId) -> bool {
+        false
+    }
+}
 
 fn main (){
-    let args: Vec<String> = env::args().collect();
-    println!("Args: {:?}", args);
+    // let args: Vec<String> = env::args().collect();
+    env_logger::init().unwrap();
 
-    let mut rpc = JsonRpc::new();
-    rpc.register("hello", Box::new(hello));
-    rpc.register("ice", Box::new(ice));
-
-    let share_rpc = Arc::new(rpc);
-    Server::http("0.0.0.0:80").unwrap().handle( MyHandler{ rpc: share_rpc.clone() } ).unwrap();    
-    // Server::http("0.0.0.0:80").unwrap().handle( move |req: Request, res: Response| {
-    //     println!("{:?}", share_rpc.methods().len() );
-    //     *res.status_mut() = StatusCode::MethodNotAllowed;
-    // }).unwrap();
+    let host = "127.0.0.1:3012";
+    Server::run(host);
 }
